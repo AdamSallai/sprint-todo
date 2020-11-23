eaecb96813178b903a62652529ea7aa1076cf0be
Adam Sallai <yadam1998@gmail.com>
2020-11-23 18:34:14 +0100

    add endpoints to backend

--- src/main/java/com/codecool/springtodo/controller/TodoController.java
+++ src/main/java/com/codecool/springtodo/controller/TodoController.java
@@ -1,11 +1,62 @@
 package com.codecool.springtodo.controller;
 
+import com.codecool.springtodo.entity.Todo;
+import com.codecool.springtodo.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Service
 @CrossOrigin
 @RestController
 public class TodoController {
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+    private static final String SUCCESS = "{\"success\":true}";
+
+    @PostMapping("/addTodo")
+    public ResponseEntity<String> addTodo() {
+        return ResponseEntity.ok(SUCCESS);
+    }
+
+    @PostMapping("/list")
+    public List<Todo> listTodos() {
+        return todoRepository.findAll();
+    }
+
+    @DeleteMapping("/todos/completed")
+    public ResponseEntity<String> completed() {
+        return ResponseEntity.ok(SUCCESS);
+    }
+
+    @PutMapping("/todos/toggle_all")
+    public ResponseEntity<String> toggleAll() {
+        return ResponseEntity.ok(SUCCESS);
+    }
+
+    @DeleteMapping("/todos/:id")
+    public ResponseEntity<String> deleteById() {
+        return ResponseEntity.ok(SUCCESS);
+    }
+
+    @PutMapping("/todos/:id")
+    public ResponseEntity<String> updateById() {
+        return ResponseEntity.ok(SUCCESS);
+    }
+
+    @GetMapping("/todos/:id")
+    public ResponseEntity<String> getById() {
+        return ResponseEntity.ok(SUCCESS);
+    }
+
+    @PutMapping("/todos/:id/toggle_status")
+    public ResponseEntity<String> toggleStatusById() {
+        return ResponseEntity.ok(SUCCESS);
+    }
+
 }
